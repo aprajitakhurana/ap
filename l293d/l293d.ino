339f1f36aca9852507545078190f3c82bef1dec7
aprajitakhurana <aprajitakhurana@gmail.com>
2019-10-23 18:57:47 +0530

    updated

--- l293d/l293d.ino
+++ l293d/l293d.ino
@@ -1,49 +1,71 @@
 
-
-int enableA=2;
-int motorA1=4;
-int motorA2=5;
-int enableB=3;
-int motorB1=6;
-int motorB2=7;
+#define motorA1 4
+#define motorA2 5
+#define motorB1 6
+#define motorB2 7
 char a;
+int w=1.57;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-pinMode(enableA,OUTPUT);
-pinMode(motorA1,OUTPUT);
-pinMode(motorA2,OUTPUT);
-pinMode(enableB,OUTPUT);
-pinMode(motorB2,OUTPUT);
-pinMode(motorB1,OUTPUT);
+//pinMode(enableA,OUTPUT);
+pinMode(4,OUTPUT);
+pinMode(5,OUTPUT);
+//pinMode(enableB,OUTPUT);
+pinMode(6,OUTPUT);
+pinMode(7,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
      //  digitalWrite(enableA,HIGH);
       //digitalWrite(enableB,HIGH);
-  switch(a)
+if(Serial.available())
+ { 
+  a=(char)Serial.read();
   
-  { case('s'):
-    {
-      digitalWrite(enableA,LOW);
-      digitalWrite(enableB,LOW);
-      delay(1000);
-    }
-    case('f'):
+  
+   
+    if(a== 'f')
     { digitalWrite(motorA1,HIGH);
       digitalWrite(motorA2,LOW);
       digitalWrite(motorB1,HIGH);
       digitalWrite(motorB2,LOW);
-      delay(3000);
+    
     }
-    case('b'):
+   if(a== 'b')
     { digitalWrite(motorA1,LOW);
       digitalWrite(motorA2,HIGH);
       digitalWrite(motorB1,LOW);
       digitalWrite(motorB2,HIGH);
-      delay(3000);
+     
     }
+    if(a== 'l')
+    { digitalWrite(motorA1,HIGH);
+      digitalWrite(motorA2,LOW);
+      digitalWrite(motorB1,LOW);
+      digitalWrite(motorB2,HIGH);
+      delay(1000);
+    }
+   if(a== 'r')
+    { digitalWrite(motorA1,LOW);
+      digitalWrite(motorA2,HIGH);
+      digitalWrite(motorB1,HIGH);
+      digitalWrite(motorB2,LOW);
+      delay(1000);
+      return;
+    }
+   if(a== 's')
+    { digitalWrite(motorA1,LOW);
+      digitalWrite(motorA2,LOW);
+      digitalWrite(motorB1,LOW);
+      digitalWrite(motorB2,LOW);
+     
+    }
+    
+    
+  } 
+    
 
-}}
+}
